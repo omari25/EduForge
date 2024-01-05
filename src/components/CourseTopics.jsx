0308874de228c4278edb6010f799d25b59527d6f
omari25 <omarmahat702@gmail.com>
2024-01-05 09:35:48 +0300

    Added a css style

--- src/components/CourseTopics.jsx
+++ src/components/CourseTopics.jsx
@@ -14,20 +14,18 @@ function CourseTopics({ courses }) {
   const topics = selectedCourse.topics
 
   return (
-    <>
-      <div className='courses-div'>
-        <h1 className='courses-title-h1'>EduForge - Courses - Topics</h1>
-        {topics.map((topic) => (
-          <Link to={`topic/${topic.id}`} key={topic.id} className='one-course-div'>
-            <img src={topic.posterFrame} alt="" />
-            <div>
-              <h2>{topic.name}</h2>
-              <p>{topic.description}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </>
+    <div className='courses-div'>
+      <h1 className='courses-title-h1'>EduForge - Courses - Topics</h1>
+      {topics.map((topic) => (
+        <Link to={`courses/topic/${topic.id}`} key={topic.id} className='one-course-div'>
+          <img src={topic.posterFrame} alt="" />
+          <div>
+            <h2>{topic.name}</h2>
+            <p>{topic.description}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
   );
 }
 
